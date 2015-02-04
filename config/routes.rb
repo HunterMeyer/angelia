50d831a912d9fcfac9df3470768eaeee571cd15a
HunterMeyer <huntermeyer@live.com>
2015-02-04 18:36:59 -0500

    Remove js default in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Angelia::Application.routes.draw do
   root 'home#index'
 
-  match '/search', to: 'home#search', via: 'post', defaults: { format: :js }
+  match '/search',  to: 'home#search',  via: 'post'
+
 end
